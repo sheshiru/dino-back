30f7497148e84ee4d2e0d598b71d1b74a70697c1
Virginie <vbouvarel@lilo.org>
2020-02-26 10:15:47 +0100

    Envoi d'email OK - I'm the nodemailer's Queen :)

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,38 +1,42 @@
-// const nodemailer = require('nodemailer');
 const Message = require ('../models/Message');
+const nodemailer = require('nodemailer');
 
-
-exports.sendMail = (req, res) => {//req = récupération des données de la requète contact
-    console.log("Message envoyé");
-
-    // let transporter = nodemailer.createTransport({
-    //     host: "smtp.gmail.com",
-    //     port: 465,
-    //     secure: true, 
-    //     service: "gmail",
-    //     auth: {
-    //       user: "virginiebouvarel@gmail.com",
-    //       pass: "47084712"
-    //     }
-    //   });
-
-    // var message = {
-    // from: "virginiebouvarel@gmail.com",
-    // to: "virginiebouvarel@gmail.com",
-    // subject: "Envoi de message d'un utilisateur",
-    // text: "salut toto",
-    // html: "<p>salut toto html</p>"
-    // };
-
-    // transporter.sendMail(message)
-    // .then(()=> console.log(req.body))
-    // .catch(err => console.error(err))   
-    
+exports.sendMail = (req, res) => {
+    //Vérification de la récupération des données
+    console.log("Données message récupérées:");
     console.log(req.body);
-    
-    let message = new Message(req.body);//création d'un nouveau message selon le modèle message, à partir des données de la requête
+
+    //Création d'un nouveau message avec les données récupérées
+    let message = new Message(req.body);
     console.log("Nouveau message:" + message);
-    message.save();//sauvegarde en base du message crée (vérifier en base)
-    res.status(201).json(message);//envoi du statut "création ok" et du corps du message (voir page response à droite)
-       
+
+    //Sauvegarde du nouveau message en base
+    message.save();
+
+    //Réponse à la requête: statut "création ok" + corps du message
+    res.status(201).json(message);
+    
+    //Paramétrage de l'envoi d'email avec nodemailer
+    var transporter = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "b97035add41e00",
+          pass: "56657d6fa53392"
+        }
+    });
+
+    //Constitution du corps du mail à envoyer 
+    var mailOptions = {
+    from: `<Pascalpes>, < pascalpes-434864@inbox.mailtrap.io>`,
+    to: `<${req.body.email}>`,
+    subject: "Envoi de message depuis le contactForm",
+    text: "Essai d'envoi message utilisateur",
+    html: "<p>Essai d'envoi message utilisateur</p>"
+    };
+
+    //Envoi du mail
+    transporter.sendMail(mailOptions)
+    .then(()=> console.log(req.body))
+    .catch(err => console.error(err)) 
 }
