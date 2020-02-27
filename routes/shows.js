7d86f600f8ba1120e6d9e9e8dcbd8d11e75f6dcc
pascalpes <pascalpes@free.fr>
2020-02-27 11:38:05 +0100

    fixing rebase front dev

--- routes/shows.js
+++ routes/shows.js
@@ -7,6 +7,7 @@ router.get("/:id", showCtrl.getShow);
 router.post("/", jsonParser, showCtrl.createShow);
 router.patch("/:id", jsonParser, showCtrl.updateShow);
 router.delete("/:id", showCtrl.deleteShow);
+
 router.patch("/add-date/:id", jsonParser, showCtrl.addDate);
 router.patch("/del-date/:id", jsonParser, showCtrl.delDate);
 router.patch("/add-resa/:id", jsonParser, showCtrl.addResa);
