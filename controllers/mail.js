3aedf9e8b5fe53edd2c32b0618920a04c92154ac
Cecile Pham <sheshiru.van@gmail.com>
2020-02-27 19:13:44 +0100

    last commit

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,42 +1,43 @@
-const Message = require ('../models/Message');
-const nodemailer = require('nodemailer');
+const Message = require("../models/Message");
+const nodemailer = require("nodemailer");
 
 exports.sendMail = (req, res) => {
-    //Vérification de la récupération des données
-    console.log("Données message récupérées:");
-    console.log(req.body);
+  //Vérification de la récupération des données
+  console.log("Données message récupérées:");
+  console.log(req.body);
 
-    //Création d'un nouveau message avec les données récupérées
-    let message = new Message(req.body);
-    console.log("Nouveau message:" + message);
+  //Création d'un nouveau message avec les données récupérées
+  let message = new Message(req.body);
+  console.log("Nouveau message:" + message);
 
-    //Sauvegarde du nouveau message en base
-    message.save();
+  //Sauvegarde du nouveau message en base
+  message.save();
 
-    //Réponse à la requête: statut "création ok" + corps du message
-    res.status(201).json(message);
-    
-    //Paramétrage de l'envoi d'email avec nodemailer
-    var transporter = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-          user: "b97035add41e00",
-          pass: "56657d6fa53392"
-        }
-    });
+  //Réponse à la requête: statut "création ok" + corps du message
+  res.status(201).json(message);
 
-    //Constitution du corps du mail à envoyer 
-    var mailOptions = {
+  //Paramétrage de l'envoi d'email avec nodemailer
+  var transporter = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "b97035add41e00",
+      pass: "56657d6fa53392"
+    }
+  });
+
+  //Constitution du corps du mail à envoyer
+  var mailOptions = {
     from: `<Pascalpes>, < pascalpes-434864@inbox.mailtrap.io>`,
     to: `<${req.body.email}>`,
     subject: "Envoi de message depuis le contactForm",
-    text: "Essai d'envoi message utilisateur",
-    html: "<p>Essai d'envoi message utilisateur</p>"
-    };
+    text: `${req.body.text}`,
+    html: `<p>${req.body.text}</p>`
+  };
 
-    //Envoi du mail
-    transporter.sendMail(mailOptions)
-    .then(()=> console.log(req.body))
-    .catch(err => console.error(err)) 
-}
+  //Envoi du mail
+  transporter
+    .sendMail(mailOptions)
+    .then(() => console.log(req.body))
+    .catch(err => console.error(err));
+};
